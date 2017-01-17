b87e7452b24d3f7be96aad584c29c0d8d72d13e1
Sridhar <sreedharriyer@gmail.com>
2017-01-17 15:00:02 -0800

    new files

---
+++ UnifiedTypes.scala
@@ -0,0 +1,11 @@
+object UnifiedTypes extends App
+  val set = new scala.collection.mutable.LinkedHashSet[Any]
+  set += "This is a string"
+  set += 732
+  set += 'c'
+  set += true
+  set += main _
+  val iter: Iterator[Any] = set.iterator
+  while(iter.hasNext) {
+    println(iter.next.toString())
+  }
---
+++ test.sc
@@ -0,0 +1,7 @@
+var str = "hello"
+println(str)
+str = "hi"
+println(str)
+// type mismatch error
+str = 1
+println(str)
