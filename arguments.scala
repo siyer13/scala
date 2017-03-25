d3e33cdefe76398b3dd6daacde6d33e5cd7139ae
Sridhar <siyer@pandora.com>
2017-03-24 20:31:08 -0700

    conscice functional style

--- arguments.scala
+++ arguments.scala
@@ -1,10 +1,12 @@
+// imperative style
 var i = 0
 while (i < args.length) {
   println(args(i))
   i += 1
 }
 
-
 // arguments in functional way
-
 args.foreach((arg: String) => println(arg))
+
+//conscice
+args.foreach(println)
