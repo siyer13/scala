caff71f5338d2850d6df11f32537d71bb93b64a9
Sridhar <siyer@pandora.com>
2017-03-26 09:30:32 -0700

    List Con

--- ListConcatenation.scala
+++ ListConcatenation.scala
@@ -4,3 +4,13 @@ val oneTwoThreeFour = oneTwo ::: threeFour
 println(oneTwo)
 println(threeFour)
 println(oneTwoThreeFour)
+
+
+// List cons :: prepends the element to the List
+
+val one = List(1)
+val two = List(2)
+val oneToo = one ::: two
+println(oneToo)
+val zeroOneTwo = 0 :: oneToo
+println(zeroOneTwo)
