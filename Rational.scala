fa71da34cf3e56121fa734b3dd7b526809ebfa49
Sridhar <sreedharriyer@gmail.com>
2017-03-28 15:08:13 -0700

    Update Rational.scala

--- Rational.scala
+++ Rational.scala
@@ -1,4 +1,7 @@
 class Rational (n: Int, d: Int) {
+    // The require method takes one boolean parameter.
+    // If the passed value is true, require will return normally.
+    // Otherwise, require will prevent the object from being constructed by throwing an IllegalArgumentException.
     require (d != 0)
     override def toString = n + " / " + d
 }
