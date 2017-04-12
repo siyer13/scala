de8293a37b1f4e10216d90439b7e3c57e20c84ff
Sridhar <sreedharriyer@gmail.com>
2017-04-12 14:08:34 -0700

    Create BankAccount.scala

---
+++ BankAccount.scala
@@ -0,0 +1,24 @@
+class BankAccount {
+  private var bal: Int = 0
+
+  def balance: Int = bal
+
+  def deposit(amount: Int): String = {
+    require(amount > 0)
+    bal += amount
+    "$"+amount+" deposited to your account"
+  }
+
+  def withdraw(amount: Int): Boolean =
+    if(amount > bal) false
+    else {
+      bal -= amount
+      true
+    }
+}
+
+
+val account = new BankAccount
+
+println(account deposit 1000)
+println(account withdraw 500)
