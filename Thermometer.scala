3595a5d224acf01c9861465e5b3d9b6ed53ae5fd
sridharriyer <sreedharriyer@gmail.com>
2017-04-12 16:15:18 -0700

    Thermometer

---
+++ Thermometer.scala
@@ -0,0 +1,15 @@
+class Thermometer {
+
+  var celsius: Float = _
+
+  def farenheit = celsius * 9 / 5 + 32
+  def farenheit_= (f: Float) {
+    celsius = (f - 32) * 5 / 9
+  }
+  override def toString = farenheit + "F/"+ celsius + "C"
+}
+
+
+val t = new Thermometer
+t. celsius = 100
+println(t)
