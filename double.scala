6eaa54ea1f774b4b34973d920d52e4544a425b9e
Sridhar <sreedharriyer@gmail.com>
2017-06-11 01:34:00 -0700

    double value

--- double.scala
+++ double.scala
@@ -1,15 +1,17 @@
-def double_value(i: Int) = i * 2
+def double_method(i: Int) = { i * 2 }
+println(double_method(2))
 
+
+val double_value = (i:Int) => { i * 2 }
 println(double_value(2))
-
-
-val db = (i:Int) => { i * 2 }
-
-println(db(2))
 
 
 val list = List.range(1,5)
 println(list)
 
-val list_double = list.map(db)
+val list_double = list.map(double_value)
 println(list_double)
+
+
+val double_double = list_double.map(double_method)
+println(double_double)
