7763a24c8bf289b5709c28ae6d630815caf003eb
Sridhar <sreedharriyer@gmail.com>
2017-06-27 09:52:50 -0700

    passing function as variable

---
+++ Demo.scala
@@ -0,0 +1,9 @@
+object Demo {
+   def main(args: Array[String]) {
+      println( apply( layout, 10) )
+   }
+
+   def apply(f: Int => String, v: Int) = f(v)
+
+   def layout[A](x: A) = "[" + x.toString() + "]"
+}
