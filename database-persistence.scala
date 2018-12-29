5153cd3b9ead05a9495c26cf0e9a8d0e85d5eaa8
Sridhar <sreedharriyer@gmail.com>
2018-12-29 22:14:30 +0530

    db code scala

--- database-persistence.scala
+++ database-persistence.scala
@@ -4,7 +4,7 @@ object ScalaJdbcConnectSelect extends App {
 
   // connect to the mysql database
 
-  val url = "jdbc:mysql://localhost:3306/test"
+  val url = "jdbc:mysql://localhost:3306/school"
   val driver = "com.mysql.jdbc.Driver"
   val username = "root"
   val password = ""
@@ -12,7 +12,16 @@ object ScalaJdbcConnectSelect extends App {
 
   try {
     Class.forName(driver)
-    connection = DriverManager.getconnection(url,username,password)
+    connection = DriverManager.getConnection(url,username,password)
     val statement = connection.createStatement
+    val rs = statement.executeQuery("select student_id, first_name from student")
+    while(rs.next) {
+      val student_id = rs.getString("student_id")
+      val first_name = rs.getString("first_name")
+      println("student_id = %s , first_name = %s".format(student_id,first_name))
+    }
+  }catch {
+    case e: Exception => e.printStackTrace
   }
+  connection.close
 }
