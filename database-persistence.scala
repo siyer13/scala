78905d84a1b2f77f464ee3be3867207752f2ecd4
Sridhar <sreedharriyer@gmail.com>
2018-12-29 22:06:25 +0530

    database scala code

--- database-persistence.scala
+++ database-persistence.scala
@@ -3,4 +3,8 @@ import java.sql.{Connection,DriverManager}
 object ScalaJdbcConnectSelect extends App {
 
   // connect to the mysql database
+
+  val url = "jdbc:mysql://localhost:3306/test"
+  val driver = "com.mysql.jdbc.Driver"
+  val username = "root"
 }
