27d02363c02f33e5d192d06dfefbf75a0b97f002
Sridhar <sreedharriyer@gmail.com>
2019-02-09 22:00:21 +0530

    Update Object_Class.scala

--- Object_Class.scala
+++ Object_Class.scala
@@ -1,4 +1,6 @@
 
+package com.imf.data
+
 object Test {
   
   def main(args : Array[String]) {
@@ -13,6 +15,17 @@ object Test {
     println(Te)
     val Cl = new TestClass()
     println(Cl)
+    
+    val m1 = Message("siyer@gmail.com", "siyer13@gmail.com", "Hello, How are you ?")
+    val m2 = Message("siyer@gmail.com", "siyer13@gmail.com", "Hello, How are you ?")
+    
+    val m = m1 == m2
+    val mm = m1.equals(m2)
+    
+    println(m)
+    println(mm)
+    
+    
   }
   
   class TestClass {
@@ -20,6 +33,11 @@ object Test {
     def someMethod() {
       
     }
+  }
+  
+  case class Message(sender: String, recepient: String, messageBody: String) {
+    
+   
   }
 }
 
@@ -33,6 +51,6 @@ com.imf.data.Test$@7b23ec81
 com.imf.data.Test$@7b23ec81
 com.imf.data.Test$TestClass@6acbcfc0
 com.imf.data.Test$TestClass@5f184fc6
-
-
+true
+true
 */
